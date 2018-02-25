60e0c809d8ac0cdd2d441203b6afbc7522fd6caa
唐伟伟 <tww24098@ly.com>
2018-02-25 14:03:46 +0800

    添加GZIP和probuf-net

--- bywaymsg/Startup.cs
+++ bywaymsg/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using bywaymsg.Formatters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Text;
+using Microsoft.Net.Http.Headers;
 
 namespace bywaymsg
 {
@@ -28,13 +31,34 @@ namespace bywaymsg
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //gzpi 压缩
+            services.AddResponseCompression();
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("PrivateCache",
+                     new Microsoft.AspNetCore.Mvc.CacheProfile
+                     {
+                         Duration = 60,
+                         Location = Microsoft.AspNetCore.Mvc.ResponseCacheLocation.None
+                     });
+                options.CacheProfiles.Add("PublicCache",
+                    new Microsoft.AspNetCore.Mvc.CacheProfile
+                    {
+                        Duration = 86400,
+                        Location = Microsoft.AspNetCore.Mvc.ResponseCacheLocation.Client
+                    });
+                options.InputFormatters.Add(new ProtobufInputFormatter());
+                options.OutputFormatters.Add(new ProtobufOutputFormatter());
+                options.FormatterMappings.SetMediaTypeMappingForFormat("protobuf", MediaTypeHeaderValue.Parse("application/x-protobuf"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseResponseCompression();
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
@@ -48,7 +72,16 @@ namespace bywaymsg
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            //使用默认文件夹wwwroot  
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    const int durationInSeconds = 60 * 60 * 24;
+                    //静态文件缓存，超过这时间 就是 304 not modified，不超过就是 200 from memory cache
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + durationInSeconds;
+                }
+            });
 
             app.UseMvc(routes =>
             {
